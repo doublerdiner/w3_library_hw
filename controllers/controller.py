7978698f133929e75d2ce8c8326f30abb3fa06a0
Chris Barclay <chris.barclay1@gmail.com>
2023-02-12 18:16:22 +0000

    Added due date functionality

--- controllers/controller.py
+++ controllers/controller.py
@@ -1,34 +1,41 @@
 from flask import render_template, request, redirect
 from app import app
-from models.book_list import book_list, delete_book, delete_book_index, add_book, check_out_book, check_book_in
+from models.book_list import *
 from models.book import Book
+from models.library import Library
+import datetime
 
 @app.route("/library")
 def index():
-    return render_template("index.html", title="Home", book_list=book_list)
+    return render_template("index.html", title="Home", book_list=library.book_list)
 
 @app.route("/library/<index>")
 def book_view(index):
     index = int(index)
-    book_to_view = book_list[index]
+    book_to_view = library.book_list[index]
     return render_template("book.html", title="Book", book=book_to_view, index=index)
 
 @app.route("/library/<index>", methods=['POST'])
 def post_update_check_status(index):
     index = int(index)
-    book_to_check_out = book_list[index]
+    book_to_check_out = library.book_list[index]
     if request.form["status"] == "CheckIn":
-        book_to_check_out.checked_out = False
+        library.check_book_in(book_to_check_out)
+        book_to_check_out.due_date = None
     else:
-        book_to_check_out.checked_out = True
+        library.check_out_book(book_to_check_out)
+        today = datetime.date.today()
+        week = datetime.timedelta(days=7)
+        book_to_check_out.due_date = today + week
+        book_to_check_out.format_due_date()
     return redirect ("/library")
 
 
 @app.route("/library/<index>/delete", methods=['POST'])
 def post_delete(index):
     index= int(index)
-    book_to_delete = book_list[index]
-    delete_book(book_to_delete)
+    book_to_delete = library.book_list[index]
+    library.delete_book(book_to_delete)
     return redirect("/library")
 
 @app.route("/library/add", methods=['POST'])
@@ -37,5 +44,5 @@ def post_add_book():
     author = request.form["author"]
     genre = request.form["genre"]
     new_book = Book(title, author, genre)
-    add_book(new_book)
+    library.add_book(new_book)
     return redirect("/library")
--- models/book.py
+++ models/book.py
@@ -1,6 +1,17 @@
+from datetime import date
 class Book:
     def __init__(self, title, author, genre):
         self.title = title
         self.author = author
         self.genre = genre
         self.checked_out = False
+        self.due_date = None
+
+    
+    def format_due_date(self):
+        date = str(self.due_date)
+        new = date.split("-")
+        year = new[0]
+        month = new[1]
+        day = new[2]
+        self.due_date = f"{day}/{month}/{year}"
