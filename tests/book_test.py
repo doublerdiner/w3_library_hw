697009ad751f25ad01300e3f216edba5906bfc71
Chris Barclay <chris.barclay1@gmail.com>
2023-02-12 19:37:04 +0000

    Completed formatting test

--- tests/book_test.py
+++ tests/book_test.py
@@ -1,5 +1,6 @@
 import unittest
 from models.book import *
+import datetime
 
 class TestBook(unittest.TestCase):
     def setUp(self):
@@ -18,4 +19,9 @@ class TestBook(unittest.TestCase):
     def test_book_has_a_check_out_status(self):
         self.assertEqual(False, self.book_1.checked_out)
 
+# Test format due date
+    def test_format_book_date(self):
+        self.book_1.due_date = datetime.date(2023, 1, 15)
+        self.book_1.format_due_date()
+        self.assertEqual("15/01/2023", self.book_1.due_date)
 
